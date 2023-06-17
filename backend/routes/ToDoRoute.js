5d58742987368fdac137f228348ee78250965899
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-06-17 19:24:03 +0400

    fixed some bugs :id

--- backend/routes/ToDoRoute.js
+++ backend/routes/ToDoRoute.js
@@ -1,8 +1,9 @@
 const {Router} = require('express')
-const {getToDo,saveToDo} = require("../controlers/ToDoControler")
+const {getToDo,saveToDo, updateToDo, deleteToDo} = require("../controlers/ToDoControler")
 const router = Router()
 
 router.get('/', getToDo)
 router.post('/save', saveToDo)
+router.delete('/delate/:_id', deleteToDo)
 
 module.exports = router;
