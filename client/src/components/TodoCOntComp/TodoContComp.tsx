19a7777bb2979e7db7217c7758beaa54a5d6b4df
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-06-19 13:19:43 +0400

    added drag and drop

--- client/src/components/TodoCOntComp/TodoContComp.tsx
+++ client/src/components/TodoCOntComp/TodoContComp.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DElateBtn, DelateImg, InpChecked, Label, Round, TodoCont, TodoItem, TodoLi } from '../styles/mainPage'
 import { Todo } from '../MainPage'
 import cross from '../../images/icon-cross.svg'
@@ -7,23 +7,55 @@ interface Props {
     toggleChecked: (index: number) => void
     deleteTodo: (todoId: string) => Promise<void>
     darkTheme:boolean
+    setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
 }
 function TodoContComp(p:Props) {
-    const {todos,toggleChecked,deleteTodo,darkTheme} = p;
+    const {todos,toggleChecked,deleteTodo,darkTheme,setTodos} = p;
+    const [draggedTodo, setDraggedTodo] = useState<Todo | null>(null);
+    const handleDragStart = (todo: Todo) => {
+      setDraggedTodo(todo);
+    };
+  
+    const handleDragEnd = () => {
+      setDraggedTodo(null);
+    };
+  
+    const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+      event.preventDefault();
+    };
+  
+    const handleDrop = (event: React.DragEvent<HTMLDivElement>, targetIndex: number) => {
+      event.preventDefault();
+  
+      if (draggedTodo) {
+        const draggedTodoIndex = todos.findIndex((todo) => todo._id === draggedTodo._id);
+        const updatedTodos = [...todos];
+        updatedTodos.splice(draggedTodoIndex, 1);
+        updatedTodos.splice(targetIndex, 0, draggedTodo);
+        setDraggedTodo(null);
+        setTodos(updatedTodos)
+      }
+    };
   return (
     <TodoCont>
           
             {todos.map((todo, index) => (
-              <TodoItem key={todo._id} checked={todo.checked} darkTheme={darkTheme}>
+              <TodoItem key={todo._id} checked={todo.checked} darkTheme={darkTheme}
+              draggable
+              onDragStart={() => handleDragStart(todo)}
+              onDragEnd={handleDragEnd}
+              onDragOver={handleDragOver}
+              onDrop={(event) => handleDrop(event, index)}
+              >
                 <Label darkTheme={darkTheme}>
                   <InpChecked
                     type="checkbox"
                     checked={todo.checked}
                     onChange={() => toggleChecked(index)}
                   />
-                  <Round darkTheme={darkTheme}></Round>
+                  <Round darkTheme={darkTheme} checked={todo.checked}></Round>
                 </Label>
-                <TodoLi darkTheme={darkTheme}>{todo.text}</TodoLi>
+                <TodoLi darkTheme={darkTheme} >{todo.text}</TodoLi>
                 <DElateBtn>
                   <DelateImg
                     src={cross}
